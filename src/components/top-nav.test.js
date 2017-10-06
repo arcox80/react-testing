5eb56d1b5bb3d7ce887ecc4c0c0ff0268d31b3b8
Andrew Cox <drew.r.cox@gmail.com>
2017-10-06 16:05:13 -0400

    Add TopNav tests

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -2,9 +2,22 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 
 import TopNav from './top-nav.js';
+import Header from './header';
 
 describe('<TopNav />', () => {
     it('Renders without crashing', () => {
         shallow(<TopNav />);
+    });
+    it('Should call InfoModal when What is clicked', () => {
+      const callback = jest.fn();
+      const wrapper = shallow(<TopNav onInfo={callback} />);
+      wrapper.find('.what').simulate('click', { preventDefault: () => {} });
+      expect(callback).toHaveBeenCalled();
+    });
+    it('Should call onNewGame when New Game is clicked', () => {
+      const callback = jest.fn();
+      const wrapper = shallow(<TopNav onNewGame={callback} />);
+      wrapper.find('.new').simulate('click', { preventDefault: () => {} });
+      expect(callback).toHaveBeenCalled();
     });
 });
