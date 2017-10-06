5ac36ddb1f13485b0a739931aad4c85c8990bffc
Andrew Cox <drew.r.cox@gmail.com>
2017-10-06 17:01:43 -0400

    Finish tests for GuessForm

--- src/components/guess-form.test.js
+++ src/components/guess-form.test.js
@@ -7,4 +7,19 @@ describe('<GuessForm />', () => {
     it('Renders without crashing', () => {
         shallow(<GuessForm />);
     });
+    it('Should fire the onGuess callback when form is submitted', () => {
+      const callback = jest.fn();
+      const wrapper = mount(<GuessForm onGuess={callback} />);
+      const value = 25;
+      wrapper.find('input[type="text"]').node.value = value;
+      wrapper.simulate('submit');
+      expect(callback).toHaveBeenCalledWith(value.toString());
+    });
+    it('Should reset the input when the form is submitted', () => {
+      const wrapper = mount(<GuessForm />);
+      const textInput = wrapper.find('input[type="text"]');
+      textInput.node.value = 25;
+      wrapper.simulate('submit');
+      expect(textInput.node.value).toEqual('');
+    });
 });
