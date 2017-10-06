f2dab33209528f11dc479ffd6952da54b2f8683e
Andrew Cox <drew.r.cox@gmail.com>
2017-10-06 17:36:35 -0400

    Finish adding tests for GuessList

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -5,7 +5,15 @@ import GuessList from './guess-list.js';
 
 describe('<GuessList />', () => {
     it('Renders without crashing', () => {
-        const guesses = [3, 4, 5];
-        shallow(<GuessList guesses={guesses} />);
+        shallow(<GuessList guesses={[]} />);
     });
+    it('Renders a list of guesses', () => {
+      const myGuesses = [3, 4, 5];
+      const wrapper = shallow(<GuessList guesses={myGuesses} />);
+      const listItems = wrapper.find('li');
+      expect(listItems.length).toEqual(myGuesses.length);
+      myGuesses.forEach((guess, index) => {
+        expect(listItems.at(index).text()).toEqual(guess.toString());
+      });
+  });
 });
