14c74cff048be9126dcfd928d9130431ff5793fb
Andrew Cox <drew.r.cox@gmail.com>
2017-10-06 13:54:13 -0400

    Finish tests for Header

--- src/components/header.test.js
+++ src/components/header.test.js
@@ -2,9 +2,20 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 
 import Header from './header.js';
+import TopNav from './top-nav';
+import InfoModal from './info-modal';
 
 describe('<Header />', () => {
     it('Renders without crashing', () => {
         shallow(<Header />);
+    });
+    it('Renders TopNav initially', () => {
+      const wrapper = shallow(<Header />);
+      expect(wrapper.containsMatchingElement(<TopNav />)).toEqual(true);
+    });
+    it('Renders InfoModal initially', () => {
+      const wrapper = shallow(<Header />);
+      wrapper.instance().toggleInfoModal(true);
+      expect(wrapper.containsMatchingElement(<InfoModal />)).toEqual(true);
     });
 });
